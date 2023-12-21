24dbeb9273e4e4a500a33ba43c5ae49721b63534
iradbouzidi <b.irad1994@gmail.com>
2023-12-21 22:05:16 +0100

    update

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure(process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID!);
   }, []);
 
   return null;
